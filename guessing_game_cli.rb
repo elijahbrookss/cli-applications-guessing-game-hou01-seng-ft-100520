37fbf94590b083b9504d66c6a1155a1b2220caca
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-23 04:59:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,8 +4,10 @@
 
 def run_guessing_game
   ran_num = rand(1..6);
+  p ran_num;
   puts("Guess a number 1 through 6");
   choice = gets.chomp;
+  p choice;
   if choice  == ran_num.to_s then
     puts("You guessed the correct number!");
   elsif choice == 'exit' then
